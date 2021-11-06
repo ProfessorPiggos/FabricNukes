3af02b859080d70d3d4a2e81ccdf2433a13f4761
ProfessorPiggos <doawk7@gmail.com>
2021-11-06 14:02:55 -0500

    not done yet but more missile

--- src/main/java/io/github/professorpiggos/fabricnukes/FabricNukes.java
+++ src/main/java/io/github/professorpiggos/fabricnukes/FabricNukes.java
@@ -1,23 +1,40 @@
 package io.github.professorpiggos.fabricnukes;
 
+import io.github.professorpiggos.fabricnukes.block.MissileLaunchpad;
 import io.github.professorpiggos.fabricnukes.entity.missiles.blackyellow.BlackYellowMissileEntity;
 import io.github.professorpiggos.fabricnukes.entity.weezer.WeezerEntity;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.block.Material;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import software.bernie.example.GeckoLibMod;
 import software.bernie.geckolib3.GeckoLib;
 
 public class FabricNukes implements ModInitializer {
-    public static final Item GUN = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item GUN = new Item(new FabricItemSettings());
+    public static final ItemGroup FABRICNUKESGROUP = FabricItemGroupBuilder.create(
+            new Identifier("fabricnukes","main"))
+            .icon(() -> new ItemStack(GUN))
+            .appendItems(stacks -> stacks.add(new ItemStack(GUN)))
+            .build();
+    public static final MissileLaunchpad MISSILE_LAUNCHPAD = new MissileLaunchpad(
+            FabricBlockSettings.of(Material.METAL)
+                    .requiresTool()
+                    .strength(5.0F, 6.0F)
+                    .sounds(BlockSoundGroup.METAL)
+    );
     public static final EntityType<WeezerEntity> WEEZER = Registry.register(
             Registry.ENTITY_TYPE,
             new Identifier("fabricnukes", "weezermob"),
@@ -32,8 +49,9 @@ public class FabricNukes implements ModInitializer {
     @Override
     public void onInitialize() {
         GeckoLib.initialize();
-        GeckoLibMod.DISABLE_IN_DEV = true;
         Registry.register(Registry.ITEM, new Identifier("fabricnukes", "gun"), GUN);
+        Registry.register(Registry.BLOCK, new Identifier("fabricnukes","missile_launchpad"), MISSILE_LAUNCHPAD);
+        Registry.register(Registry.ITEM, new Identifier("fabricnukes", "missile_launchpad"), new BlockItem(MISSILE_LAUNCHPAD, new FabricItemSettings().group(FABRICNUKESGROUP)));
         FabricDefaultAttributeRegistry.register(WEEZER, WeezerEntity.weezerDefaultAttributes());
         FabricDefaultAttributeRegistry.register(BLACK_YELLOW_MISSILE, BlackYellowMissileEntity.blackYellowMissileDefaultAttributes());
     }
---
+++ src/main/java/io/github/professorpiggos/fabricnukes/block/MissileLaunchpad.java
@@ -0,0 +1,9 @@
+package io.github.professorpiggos.fabricnukes.block;
+
+import net.minecraft.block.Block;
+
+public class MissileLaunchpad extends Block {
+    public MissileLaunchpad(Settings settings) {
+        super(settings);
+    }
+}
