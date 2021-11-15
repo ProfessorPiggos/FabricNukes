71c1e81f8e07cddfbb8208ef4dfa21d7cd8ef0c3
Ethan Sanders <doawk7@gmail.com>
2021-11-15 15:14:23 -0600

    inventory

--- src/main/java/io/github/professorpiggos/fabricnukes/block/missilelaunchpad/MissileLaunchpad.java
+++ src/main/java/io/github/professorpiggos/fabricnukes/block/missilelaunchpad/MissileLaunchpad.java
@@ -1,21 +1,27 @@
 package io.github.professorpiggos.fabricnukes.block.missilelaunchpad;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
-public class MissileLaunchpad extends Block implements BlockEntityProvider {
+public class MissileLaunchpad extends BlockWithEntity implements BlockEntityProvider {
     public MissileLaunchpad(Settings settings) {
         super(settings);
     }
-
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
     public static final Identifier ID = new Identifier("fabricnukes","missile_launchpad");
 
     @SuppressWarnings("deprecation")
@@ -27,5 +33,13 @@ public class MissileLaunchpad extends Block implements BlockEntityProvider {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new MissileLaunchpadEntity(pos, state);
+    }
+    @SuppressWarnings("deprecation")
+    @Override
+    public ActionResult onUse(@NotNull BlockState state, World world, BlockPos pos, @NotNull PlayerEntity player, Hand hand, BlockHitResult hit) {
+        // You need a Block.createScreenHandlerFactory implementation that delegates to the block entity,
+        // such as the one from BlockWithEntity
+        player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+        return ActionResult.SUCCESS;
     }
 }
