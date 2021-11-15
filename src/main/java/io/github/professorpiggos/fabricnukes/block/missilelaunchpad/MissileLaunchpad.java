f293070fcf6c86cc36f984a18abd59b19fda8208
ProfessorPiggos <doawk7@gmail.com>
2021-11-14 19:22:32 -0600

    in theory it should actually open now

--- src/main/java/io/github/professorpiggos/fabricnukes/block/missilelaunchpad/MissileLaunchpad.java
+++ src/main/java/io/github/professorpiggos/fabricnukes/block/missilelaunchpad/MissileLaunchpad.java
@@ -5,11 +5,16 @@ import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class MissileLaunchpad extends Block implements BlockEntityProvider {
     public MissileLaunchpad(Settings settings) {
@@ -22,6 +27,13 @@ public class MissileLaunchpad extends Block implements BlockEntityProvider {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 2.0f, 1.125f);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+        return ActionResult.SUCCESS;
     }
 
     @Override
