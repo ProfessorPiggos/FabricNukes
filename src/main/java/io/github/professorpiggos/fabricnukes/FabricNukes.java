d1ccf5fc01611355ede6fff69cbf2f825a7da42b
ProfessorPiggos <doawk7@gmail.com>
2021-11-17 09:25:44 -0600

    undoing error checking

--- src/main/java/io/github/professorpiggos/fabricnukes/FabricNukes.java
+++ src/main/java/io/github/professorpiggos/fabricnukes/FabricNukes.java
@@ -7,7 +7,7 @@ import io.github.professorpiggos.fabricnukes.entity.missiles.blackyellow.BlackYe
 import io.github.professorpiggos.fabricnukes.entity.missiles.jonarbuckle.JonArbuckleMissileEntity;
 import io.github.professorpiggos.fabricnukes.entity.weezer.WeezerEntity;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;jksdfjkads
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
